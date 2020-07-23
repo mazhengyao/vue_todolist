b73f02e7d84a3673760206741df7c543019d3c60
mazhengyao <ma_zhengyao@163.com>
2020-07-23 16:08:32 +0800

    init

---
+++ src/TodoList.vue
@@ -0,0 +1,69 @@
+<!--<template>-->
+<!--  <div id="app">-->
+<!--    <img src="./assets/logo.png">-->
+<!--    <HelloWorld/>-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--import HelloWorld from './components/HelloWorld'-->
+
+<!--export default {-->
+<!--  name: 'App',-->
+<!--  components: {-->
+<!--    HelloWorld-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
+
+<!--<style>-->
+<!--#app {-->
+<!--  font-family: 'Avenir', Helvetica, Arial, sans-serif;-->
+<!--  -webkit-font-smoothing: antialiased;-->
+<!--  -moz-osx-font-smoothing: grayscale;-->
+<!--  text-align: center;-->
+<!--  color: #2c3e50;-->
+<!--  margin-top: 60px;-->
+<!--}-->
+<!--</style>-->
+<template>
+  <div>
+    <div>
+      <input class="item" v-model="inputValue"/>
+      <button @click="handleSubmit">提交</button>
+    </div>
+    <ul>
+      <todo-item v-for="(item, index) of list" :content="item"
+                 :index="index" :key="index" @delete="handleDelete"></todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem'
+export default {
+  components : {
+      'todo-item':TodoItem,
+  },
+  data () {
+      return {
+        inputValue: '',
+        list: []
+      }
+  },
+  methods: {
+    handleSubmit () {
+      this.list.push(this.inputValue)
+      this.inputValue = ''
+    },
+    handleDelete (index) {
+      this.list.splice(index, 1);
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <li class="item" @click="handleDelete">{{content}}</li>
+</template>
+
+<script>
+export default {
+  props: ['content', 'index'],
+  methods:{
+    handleDelete (){
+      this.$emit('delete', this.index);
+    }
+  }
+}
+</script>
+
+<!--作用域-->
+<style scoped>
+  .item {
+    color: blue;
+  }
+</style>
